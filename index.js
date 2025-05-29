ab6ff9b4a1f3c2233ed234789f9cac00a6b9bcbd
mughira0 <mughiraubaid432@gmail.com>
2025-05-29 15:55:00 +0500

    chnages

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("/api/v1/blogs", blogsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
